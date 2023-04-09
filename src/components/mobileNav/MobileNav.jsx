9f32b1a96bf17b15438743429b30fced048a6de9
Francisco Neto <wantedshark@gmail.com>
2023-04-09 10:36:29 -0300

    mobile nav and hostTypes style improved

--- src/components/mobileNav/MobileNav.jsx
+++ src/components/mobileNav/MobileNav.jsx
@@ -63,13 +63,13 @@ export default function MobNav() {
                 <ul className="mobNav-items-container">
                     <li>
                         <Link to="/" className="mobNav-item">
-                            <AiOutlineSearch size={30} className={`${pathname === "/" && "mobNav-item-selected"}`} />
+                            <AiOutlineSearch size={25} className={`${pathname === "/" && "mobNav-item-selected"}`} />
                             <span className={`${pathname === "/" && "mobNav-item-selected--text"}`} >Explorar</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="/favorites" className="mobNav-item">
-                            <AiOutlineHeart size={30} className={`${pathname === "/favorites" && "mobNav-item-selected"}`} />
+                            <AiOutlineHeart size={25} className={`${pathname === "/favorites" && "mobNav-item-selected"}`} />
                             <span className={`${pathname === "/favorites" && "mobNav-item-selected--text"}`} >Favoritos</span>
                         </Link>
                     </li>
@@ -77,26 +77,26 @@ export default function MobNav() {
                         <>
                             <li>
                                 <Link to="/travels" className="mobNav-item">
-                                    <AiOutlineCar size={30} className={`${pathname === "/travels" && "mobNav-item-selected"}`} />
+                                    <AiOutlineCar size={25} className={`${pathname === "/travels" && "mobNav-item-selected"}`} />
                                     <span className={`${pathname === "/travels" && "mobNav-item-selected--text"}`} >Viagens</span>
                                 </Link>
                             </li>
                             <li>
                                 <Link to="/messages" className="mobNav-item">
-                                    <AiOutlineMessage size={30} className={`${pathname === "/messages" && "mobNav-item-selected"}`} />
+                                    <AiOutlineMessage size={25} className={`${pathname === "/messages" && "mobNav-item-selected"}`} />
                                     <span className={`${pathname === "/messages" && "mobNav-item-selected--text"}`} >Mensagens</span>
                                 </Link>
                             </li>
                             <li onClick={handleLogout}>
                                 <Link to="/profile" className="mobNav-item">
-                                    <AiOutlineUser size={30} className={`${pathname === "/profile" | pathname === "/account" && "mobNav-item-selected"}`} />
+                                    <AiOutlineUser size={25} className={`${pathname === "/profile" | pathname === "/account" && "mobNav-item-selected"}`} />
                                     <span className={`${pathname === "/profile" | pathname === "/account" && "mobNav-item-selected--text"}`} >Perfil</span>
                                 </Link>
                             </li>
                         </> :
                         <li onClick={handleLogin}>
                             <Link to={"/login"} className="mobNav-item">
-                                <AiOutlineUser size={30} className="mobNav-item" />
+                                <AiOutlineUser size={25} className="mobNav-item" />
                                 <span className="">Entrar</span>
                             </Link>
                         </li>
