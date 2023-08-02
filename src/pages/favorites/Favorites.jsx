8a07a953e8ceb8000c28696862d82dcf60ce7f68
Francisco Neto <wantedshark@gmail.com>
2023-08-02 11:04:25 -0300

    favorites container issue improved

--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux"
 import "./favorites.css"
+import { useEffect } from "react"
 export default function Favorites() {
+    const favorites = useSelector(data => data.app.favorites)
+    useEffect(() => {
+        console.log(favorites)
+    },[])
     return (
         <div className="favorites">
             <h1>favorites</h1>
