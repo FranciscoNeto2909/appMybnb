a430b0d55b8fb28ccdbc886b1a92aa077b0fdadc
Francisco Neto <wantedshark@gmail.com>
2023-08-02 22:25:11 -0300

    profile style improved

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -2,13 +2,13 @@ import React, { useEffect } from "react"
 import { AiOutlineRight, AiOutlineSolution, AiOutlineSetting, AiOutlineHome, AiOutlineControl, AiOutlineBuild, AiOutlineGift, AiOutlineQuestion, AiOutlineGlobal } from "react-icons/ai"
 import { Link, useNavigate } from "react-router-dom"
 import "./profile.css"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { logout } from "../../assets/userSlice"
 export default function Profile({ windowWidth }) {
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const userAcomodations = [1, 2]
-    const userName = "User"
+    const user = useSelector(data => data.user.user)
     const userImg = "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-File.png"
 
     function handleLogout() {
@@ -30,62 +30,58 @@ export default function Profile({ windowWidth }) {
                 <div className="profile-profile">
                     <img src={userImg} className="profile-profile-img" alt="" />
                     <div className="profile-profile-texts">
-                        <span className="profile-profile-texts-title">{userName}</span>
+                        <span className="profile-profile-texts-title">{user.name}</span>
                         <span className="profile-profile-texts-desc">Mostrar perfil</span>
                     </div>
-                    <AiOutlineRight size={20} className="profile-arowLeft" />
+                    <AiOutlineRight size={18} className="profile-arowLeft" />
                 </div>
                 <Link to="/account/personal-infos" className="profile-option" >
-                    <AiOutlineSolution size={22} />
+                    <AiOutlineSolution size={24} />
                     <span className="profile-option-text">Informações pessoais</span>
-                    <AiOutlineRight size={20} className="profile-arowLeft" />
+                    <AiOutlineRight size={18} className="profile-arowLeft" />
                 </Link>
                 <Link to="/account" className="profile-option">
-                    <AiOutlineSetting sie={22} />
+                    <AiOutlineSetting sie={24} />
                     <span className="profile-option-text">Conta</span>
-                    <AiOutlineRight size={20} className="profile-arowLeft" />
+                    <AiOutlineRight size={18} className="profile-arowLeft" />
                 </Link>
-                <hr />
             </section>
             <section className="profile-divisor">
-                <h3>Hospedagem</h3>
+                <h3 className="profile-title">Hospedagem</h3>
                 <Link className="profile-option">
-                    <AiOutlineHome size={22} />
+                    <AiOutlineHome size={24} />
                     <span className="profile-option-text">Hospede uma acomodação</span>
-                    <AiOutlineRight size={20} className="profile-arowLeft" />
+                    <AiOutlineRight size={18} className="profile-arowLeft" />
                 </Link>
                 {userAcomodations.length > 0 &&
                     <Link className="profile-option">
-                        <AiOutlineControl size={22} />
+                        <AiOutlineControl size={24} />
                         <span className="profile-option-text">Minhas acomodações</span>
-                        <AiOutlineRight size={20} className="profile-arowLeft" />
+                        <AiOutlineRight size={18} className="profile-arowLeft" />
                     </Link>}
                 <Link className="profile-option">
-                    <AiOutlineBuild size={22} />
+                    <AiOutlineBuild size={24} />
                     <span className="profile-option-text">Hospede uma experiência</span>
-                    <AiOutlineRight size={20} className="profile-arowLeft" />
+                    <AiOutlineRight size={18} className="profile-arowLeft" />
                 </Link>
-                <hr />
             </section>
             <section className="profile-divisor">
-                <h3>Indicações e créditos</h3>
+                <h3 className="profile-title">Indicações e créditos</h3>
                 <Link className="profile-option">
-                    <AiOutlineGift size={22} />
+                    <AiOutlineGift size={24} />
                     <span className="profile-option-text">Indicar um anfitrião</span>
-                    <AiOutlineRight size={20} className="profile-arowLeft" />
+                    <AiOutlineRight size={18} className="profile-arowLeft" />
                 </Link>
-                <hr />
             </section>
             <section className="profile-divisor">
-                <h3>Atendimento</h3>
+                <h3 className="profile-title">Atendimento</h3>
                 <Link className="profile-option">
                     <AiOutlineQuestion size={22} />
                     <span className="profile-option-text">Como funciona o mybnb</span>
-                    <AiOutlineRight size={20} className="profile-arowLeft" />
+                    <AiOutlineRight size={18} className="profile-arowLeft" />
                 </Link>
-                <hr />
             </section>
-            <section className="profile-divisor">
+            <section className="profile-divisor profile-divisor--noborder">
                 <div className="profile-buttons">
                     <button className="profile-buttons-button-lang">
                         <AiOutlineGlobal size={22} />
